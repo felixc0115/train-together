2ee8c60f038b224ed627719e5d3810880e8f2203
Felix Chen <fechen1993@gmail.com>
2023-03-07 13:00:45 -0500

    create http request functionalities

--- src/server/db/conn.js
+++ src/server/db/conn.js
@@ -1,7 +1,5 @@
 const { MongoClient } = require("mongodb");
-
 const Db = process.env.ATLAS_URI;
-
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,13 +10,16 @@ var _db;
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
+      console.log("hello");
+      // Verify we got a good "db" object
       if (db) {
         _db = db.db("employees");
-        console.log("successfully connected to MongoDB.");
+        console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
     });
   },
+
   getDb: function () {
     return _db;
   },
