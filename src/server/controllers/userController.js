1075b30e96b412cc6125d0c91c6e5e9201d37b17
Felix Chen <fechen1993@gmail.com>
2023-03-18 11:23:43 -0400

    work on createToken function for jwtoken

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = async (req, res) => {
