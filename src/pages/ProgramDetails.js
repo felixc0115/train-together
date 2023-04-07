83883928c855105fb25525d9a37cfb6581ba6505
Felix Chen <fechen1993@gmail.com>
2023-04-06 22:25:30 -0400

    create add program button on programs page

--- src/pages/ProgramDetails.js
+++ src/pages/ProgramDetails.js
@@ -1,4 +1,3 @@
-import YoutubeEmbed from "../components/YoutubeEmbed";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -10,24 +9,38 @@ const ProgramDetailsPage = () => {
   const program = programs.find((program) => program._id === programId);
 
   return (
-    <div>
-      <h1>{program.title}</h1>
-      <p>
-        Uploaded by: {program.username || "felix"} | category:{" "}
-        {program.category} | duration: {program.durationInMins} mins | 20 people
-        training
-      </p>
-      <YoutubeEmbed embedId={program.youtubeLink.split("=")[1]} />
-      <h2>Exercises:</h2>
-      <ul>
-        {program.exercises.map((exercise, index) => (
-          <li key={index}>
-            {exercise.name}: {exercise.sets}x for{" "}
-            {exercise.repsOrDurationPerSet} {`(${exercise.timestamp})`}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="flex mx-auto my-7 flex-wrap justify-center prose prose-headings:font-serif prose-headings:text-black-700">
+        <h1>{program.title}</h1>
+        <p>
+          Uploaded by: {program.username} <strong>|</strong> category:{" "}
+          {program.category} <strong>|</strong> duration:{" "}
+          {program.durationInMins} mins <strong>|</strong> 20 people training
+        </p>
+      </div>
+      <div className="overflow-hidden pb-32  h-full relative ">
+        <iframe
+          className="top-0 left-0 h-4/5 w-4/5 absolute"
+          src={`https://www.youtube.com/embed/${
+            program.youtubeLink.split("=")[1]
+          }`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </div>
+      <div>
+        <h2>Exercises:</h2>
+        <ul>
+          {program.exercises.map((exercise, index) => (
+            <li key={index}>
+              {exercise.name}: {exercise.sets}x for{" "}
+              {exercise.repsOrDurationPerSet} {`(${exercise.timestamp})`}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -14,11 +14,88 @@ const ProgramsPage = () => {
   }, [dispatch, token]);
 
   return (
-    <div className="flex w-full gap-5 mx-auto my-7 flex-wrap justify-center">
-      {allPrograms.map((program) => (
-        <ProgramCard key={program["_id"]} program={program} />
-      ))}
-    </div>
+    <>
+      <div>
+        <label htmlFor="my-modal-3" className="btn">
+          add program
+        </label>
+
+        <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box relative">
+            <label
+              htmlFor="my-modal-3"
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+            >
+              ✕
+            </label>
+            <h3 className="text-lg font-bold">
+              Congratulations random Internet user!
+            </h3>
+            <div className="form-control mb-3">
+              <label className="input-group input-group-s">
+                <span>program title</span>
+                <input
+                  // ref={titleRef}
+                  type="text"
+                  placeholder="hip mobility routine"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+            <div className="form-control mb-3">
+              <label className="input-group input-group-s">
+                <span>program description</span>
+                <input
+                  // ref={descriptionRef}
+                  type="text"
+                  placeholder="mobility routine to work on your hips!"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+            <div className="form-control mb-3">
+              <label className="input-group input-group-s">
+                <span>youtube link</span>
+                <input
+                  // ref={youtubeLinkRef}
+                  type="text"
+                  placeholder="https://www.youtube.com/watch?v=jj2AAH6jbHk"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+            <div className="form-control mb-3">
+              <label className="input-group input-group-s">
+                <span>program category</span>
+                <input
+                  // ref={categoryRef}
+                  type="text"
+                  placeholder="(e.g., mobility, stretching, workout, rehab)"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+            <div className="form-control mb-3">
+              <label className="input-group input-group-s">
+                <span>program duration (mins)</span>
+                <input
+                  // ref={durationRef}
+                  type="text"
+                  placeholder="15"
+                  className="input input-bordered"
+                />
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full gap-5 mx-auto my-7 flex-wrap justify-center">
+        {allPrograms.map((program) => (
+          <ProgramCard key={program["_id"]} program={program} />
+        ))}
+      </div>
+    </>
   );
 };
 
