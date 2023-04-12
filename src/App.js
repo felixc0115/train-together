618fa740d5d4ae8a3e534002aedbfa4c736d6523
Felix Chen <fechen1993@gmail.com>
2023-04-11 20:14:50 -0400

    fix create account page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ProgramsPage from "./pages/Programs";
 import MyProgramsPage from "./pages/MyPrograms";
 import ProgramDetailsPage from "./pages/ProgramDetails";
 import LoginPage from "./pages/Login";
-import CreateAccountPage from "./pages/CreateAccount.jsx";
+import CreateAccountPage from "./pages/CreateAccount";
 
 function App() {
   const user = useSelector((state) => state.auth.user);
