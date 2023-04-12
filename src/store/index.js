d6a5c07a216189ba51b29276b8f347e379611a97
Felix Chen <fechen1993@gmail.com>
2023-04-12 09:27:44 -0400

    fix es lint warnings

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import allProgramsReducer from "./all-programs-slice";
 import storage from "redux-persist/lib/storage";
