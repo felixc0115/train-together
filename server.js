2c9f0faed767a0da341c5881cff46a0f12d4645a
Felix Chen <fechen1993@gmail.com>
2023-04-16 12:56:23 -0400

    add cross origin logic to server.js

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
