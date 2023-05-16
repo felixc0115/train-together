0691e4f510c9e850efacc024e64ef60ec8ce58cc
Felix Chen <fechen1993@gmail.com>
2023-05-16 11:05:02 -0400

    implement Filter component in Programs page

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,10 +1,14 @@
-const Filter = () => {
+const Filter = ({ filterHandler }) => {
   return (
     <div className="flex justify-end">
-      <select className="select select-bordered w-full max-w-xs mb-3 mr-10">
+      <select
+        onChange={filterHandler}
+        className="select select-bordered w-full max-w-xs mb-3 mr-10"
+      >
         <option disabled selected>
           filter by program type
         </option>
+        <option value="show all">show all</option>
         <option value="mobility">mobility</option>
         <option value="stretching">stretching</option>
         <option value="workout">workout</option>
--- frontend/src/pages/Programs.js
+++ frontend/src/pages/Programs.js
@@ -1,6 +1,6 @@
 import ProgramCard from "../components/ProgramCard";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAllProgramsData } from "../store/program-actions";
 import AddProgramButton from "../components/AddProgramButton";
 import Filter from "../components/Filter";
@@ -8,6 +8,7 @@ import Filter from "../components/Filter";
 const ProgramsPage = () => {
   const allPrograms = useSelector((state) => state.allPrograms.programs);
   const { token } = useSelector((state) => state.auth.user);
+  const [filter, setFilter] = useState("");
 
   const dispatch = useDispatch();
 
@@ -15,15 +16,23 @@ const ProgramsPage = () => {
     dispatch(fetchAllProgramsData(token));
   }, [dispatch, token]);
 
+  const filterHandler = (e) => {
+    e.target.value === "show all" ? setFilter(null) : setFilter(e.target.value);
+  };
+
+  const programsToShow = !filter
+    ? allPrograms
+    : allPrograms.filter((program) => program.category === filter);
+
   return (
     <>
       <h2 className="text-center font-bold normal-case text-2xl mb-2">
         All Community Uploaded Programs
       </h2>
-      <Filter />
+      <Filter filterHandler={filterHandler} />
       <AddProgramButton />
       <div className="flex w-full gap-5 mx-auto flex-wrap justify-center">
-        {allPrograms.map((program) => (
+        {programsToShow.map((program) => (
           <ProgramCard key={program["_id"]} program={program} />
         ))}
       </div>
