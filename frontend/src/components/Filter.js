96ad332e107a92b292033e5d4488e0c369efabd7
Felix Chen <fechen1993@gmail.com>
2023-05-16 10:50:19 -0400

    finish Filter component

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,15 +1,15 @@
 const Filter = () => {
   return (
-    <>
-      <label for="program-select">Filter by type:</label>
-
-      <select name="types" id="program-select">
-        <option value="">--Please choose an option--</option>
+    <div className="flex justify-end">
+      <select className="select select-bordered w-full max-w-xs mb-3 mr-10">
+        <option disabled selected>
+          filter by program type
+        </option>
         <option value="mobility">mobility</option>
         <option value="stretching">stretching</option>
         <option value="workout">workout</option>
       </select>
-    </>
+    </div>
   );
 };
 
